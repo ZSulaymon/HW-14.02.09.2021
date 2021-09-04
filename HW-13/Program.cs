68e630c9b7b569c603bb5f3f5562374a0397b232
SulaymonZafarov <sulaymonwork@gmail.com>
2021-09-04 17:05:36 +0500

    Add project files.

---
+++ HW-13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HW_13
+{
+    class Tiger
+    {
+        public string Name;
+        public int Age;
+        public string Hieght;
+        public string Weight;
+        public string Color;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
